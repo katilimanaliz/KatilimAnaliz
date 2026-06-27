0ef1f71f9822c8aae4932eadbad74943c6b9e9a6
katilimanaliz <katilimanalizz@gmail.com>
2026-06-27 19:13:52 +0300

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'window.__ANTHROPIC_KEY__': JSON.stringify(process.env.VITE_ANTHROPIC_KEY || ''),
+  },
 })
+
