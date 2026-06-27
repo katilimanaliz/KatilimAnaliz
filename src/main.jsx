a4e6cc28ae5ab850e2109af69ce955d276aa04df
katilimanaliz <katilimanalizz@gmail.com>
2026-06-28 01:32:16 +0300

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '../FiyatlamaPro.tsx'
+import { Analytics } from '@vercel/analytics/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
- <React.StrictMode>
-   <App />
- </React.StrictMode>
+  <React.StrictMode>
+    <App />
+    <Analytics />
+  </React.StrictMode>
 )
 
