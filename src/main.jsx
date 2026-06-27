386802eba86ee73c333d62c52ac7f097781a219a
katilimanaliz <katilimanalizz@gmail.com>
2026-06-27 18:27:46 +0300

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './FiyatlamaPro.tsx'
+import App from '../FiyatlamaPro.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
